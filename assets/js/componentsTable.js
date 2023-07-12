88b9b965708f687be352c28b32a20ffb669651ff
Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
2023-07-12 16:33:42 +0100

    HEAT-90: Display product name and id on component list screen (#20)

--- assets/js/componentsTable.js
+++ assets/js/componentsTable.js
@@ -23,6 +23,24 @@ jQuery(function () {
           $(td).html(`<a href="/components/${rowData.id}">${rowData.attributes.name}</a>`)
         },
       },
+      {
+        data: 'attributes.product.data.id',
+        createdCell: function (td, cellData, rowData) {
+          const link = rowData.attributes.product.data
+            ? `<a href="/products/${rowData.attributes.product.data.id}">${rowData.attributes.product.data.attributes.p_id}</a>`
+            : 'N/A'
+          $(td).html(link)
+        },
+      },
+      {
+        data: 'attributes.product.data.id',
+        createdCell: function (td, cellData, rowData) {
+          const link = rowData.attributes.product.data
+            ? `<a href="/products/${rowData.attributes.product.data.id}">${rowData.attributes.product.data.attributes.name}</a>`
+            : 'N/A'
+          $(td).html(link)
+        },
+      },
     ],
   })
 })
