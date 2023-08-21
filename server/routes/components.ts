6d41ed11aa27813da7d916bcc802372c02ced629
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2023-08-21 15:15:22 +0100

    Fix linting

--- server/routes/components.ts
+++ server/routes/components.ts
@@ -1,12 +1,12 @@
 import { type RequestHandler, type Request, Router } from 'express'
 import { BadRequest } from 'http-errors'
-import { forever } from 'async'
-import { commandOptions } from 'redis'
+// import { forever } from 'async'
+// import { commandOptions } from 'redis'
 import asyncMiddleware from '../middleware/asyncMiddleware'
 import type { Services } from '../services'
 import type { Environment } from '../data/strapiApiTypes'
-import { createRedisClient } from '../data/redisClient'
-import logger from '../../logger'
+// import { createRedisClient } from '../data/redisClient'
+// import logger from '../../logger'
 
 export default function routes({ serviceCatalogueService }: Services): Router {
   const router = Router()
