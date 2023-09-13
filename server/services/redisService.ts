b57b7815df7dd2c7aa43ef2baa3c2b56379dbf24
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2023-09-13 10:50:55 +0100

    Add timeout to stream calls

--- server/services/redisService.ts
+++ server/services/redisService.ts
@@ -16,16 +16,13 @@ export default class RedisService {
       id: string
     }[],
   ): Promise<string> {
-    // ): Promise<RedisStreamMessage[] | null> {
     try {
       const response = await this.redisClient.xRead(
         commandOptions({ isolated: true }), // uses new connection from pool not to block other redis calls
         streamDetails,
       )
 
-      // const messages: RedisStreamMessage[] = response ? response[0].messages : [] // returning first stream (since only 1 stream used)
       return response ? JSON.stringify(response) : ''
-      // return messages || []
     } catch (error) {
       if (error instanceof ClientClosedError) {
         logger.error(`${error.message} ...RECONNECTING`)
