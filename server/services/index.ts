fb3a940f777b798f0ae61b6c77490e6ca3734722
Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
2023-09-13 12:56:22 +0100

    HEAT-107: Subscribe to Redis streams (#35)
    * Update node modules
    
    * HEAT-107: Subscribe to Redis streams
    
    * Fix linting
    
    * Fetch queue from front-end
    
    * Output messages to console
    
    * FIx first message ID
    
    * Fix plus more debug
    
    * Fix lint
    
    * Push CircleCI
    
    * Push CircleCI
    
    * Send object
    
    * Revert
    
    * Remove async await
    
    * Make fetch async await, add try/catch
    
    * Log some debug info to check queue calls
    
    * Try awaiting json
    
    * Parse messages
    
    * Fix node version
    
    * Fix var name
    
    * Fix data
    
    * Debugging
    
    * Check key
    
    * Use jQuery
    
    * Push CircleCI
    
    * Check stream type
    
    * Parse status
    
    * Check JSON
    
    * Fix JSON parse
    
    * Fix JSON parse
    
    * Use new var
    
    * Rearrange env table
    
    * Make env name clickable, remove URL field
    
    * Start with latest message
    
    * Sort message counts
    
    * Fix missing code
    
    * Check status
    
    * Sort JSOn parse
    
    * Trap JSON parse
    
    * Update package files
    
    * Add timeout to stream calls
    
    * Tidy responses
    
    * Add jsondata output, try fix status
    
    * Remove console logs
    
    * Fix status

--- server/services/index.ts
+++ server/services/index.ts
@@ -1,17 +1,24 @@
 import { dataAccess } from '../data'
 import ServiceCatalogueService from './serviceCatalogueService'
+import RedisService from './redisService'
+import { createRedisClient } from '../data/redisClient'
+import logger from '../../logger'
 
 export const services = () => {
   const { strapiApiClientBuilder, applicationInfo } = dataAccess()
+  const client = createRedisClient()
+  client.connect().catch((err: Error) => logger.error(`Error connecting to Redis`, err))
 
   const serviceCatalogueService = new ServiceCatalogueService(strapiApiClientBuilder)
+  const redisService = new RedisService(client)
 
   return {
     applicationInfo,
     serviceCatalogueService,
+    redisService,
   }
 }
 
 export type Services = ReturnType<typeof services>
 
-export { ServiceCatalogueService }
+export { ServiceCatalogueService, RedisService }
