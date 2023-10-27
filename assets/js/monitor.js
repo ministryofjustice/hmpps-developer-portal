b225e2d8819d91e7ae5e479884fa49e69d932eb6
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2023-10-27 16:12:28 +0100

    Add time ago

--- assets/js/monitor.js
+++ assets/js/monitor.js
@@ -1,5 +1,6 @@
 const lastIds = {}
 const data = {}
+dayjs.extend(window.dayjs_plugin_relativeTime)
 
 jQuery(async function () {
   const monitorType = $('#monitorType').val()
@@ -112,7 +113,7 @@ const fetchMessages = async streams => {
             break
           case 'h':
             const lastMessageTime = new Date(parseInt(lastMessage.id.split('-')[0]))
-            const dateString = lastMessageTime.toLocaleString()
+            const dateString = dayjs().to(dayjs(lastMessageTime))
             $(`#tile-${component}-${environment} .statusTileLastRefresh`).text(dateString)
             try {
               if (data[streamName].json) {
