7cfea0ba72c9bb5e03431960c09a229d63fd14c3
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2023-11-27 09:58:01 +0000

    Make text clearer by not changing colour over text (#108)
    * Make text clearer by not changing colour over text
    
    * Tweaking design
    
    * Showing many radiators at once

--- assets/js/driftRadiator.js
+++ assets/js/driftRadiator.js
@@ -1,54 +1,76 @@
 dayjs.extend(window.dayjs_plugin_relativeTime)
 
-const lastIds = Object.fromEntries(environments.map(e => [`v:${e}`, 0]))
-const data = Object.fromEntries(environments.map(e => [`${e}`, undefined]))
+const lastIds = Object.fromEntries(components.flatMap(c => c.environments.map(e => [e.streamName, '0'])))
+const envs = Object.fromEntries(components.flatMap(c => c.environments.map(e => [e.id, e])))
 
 const tileLine = (label, value) => `<strong>${label}:</strong><span class="tile-value">${value}</span><br/>`
-const tileLink = (title, href) => `<a class="govuk-link--no-visited-state" href="${href}">${title}</a>`
+const tileLink = (title, href) => `<a class="tile-value govuk-link--no-visited-state" href="${href}">${title}</a>`
 
-const updateVersion = ({ env, version }) => {
-  const [date, build, sha] = version.split('.')
-  data[env] = { version, date, build, sha }
+const getDaysDiff = (thisEnv, otherEnv) => {
+  const otherEnvDate = otherEnv.version.date
+  const thisEnvDate = thisEnv.version.date
 
-  $(`#${env}_details`).html(`
-    ${tileLine('Environment', env)}
-    ${tileLine('Build date', date)}
-    ${tileLine('Build number', build)}`)
+  return parseInt(thisEnv.version.sha === otherEnv.version.sha ? -1 : dayjs(otherEnvDate).diff(thisEnvDate, 'day'), 10)
+}
 
-  if (env != devEnvName && data[devEnvName]) {
-    const devEnvSha = data[devEnvName].sha
-    const thisEnvSha = data[env].sha
+const getIndicatorColour = daysDiff => {
+  // Decrease denominator to increase rate of change , increase daysDiff to increase initial jump
+  const diffScore = (daysDiff + 6) / 15
 
-    const devEnvDate = data[devEnvName].date
-    const thisEnvDate = data[env].date
+  const hue = Math.max(0, (1 - diffScore) * 120).toString(10)
+  return `hsl(${hue},50%,50%)`
+}
 
-    const daysDiff = devEnvSha === thisEnvSha ? -1 : dayjs(devEnvDate).diff(thisEnvDate, 'day')
+const updateVersion = (env, devEnv, prodEnv) => {
+  $(`#${env.id}_name`).html(tileLine(env.name, 'Most recent build deployed'))
+  $(`#${env.id}_details`).html(tileLine('Version', env.version.full))
 
-    // Decrease denominator to increase rate of change , increase daysDiff to increase initial jump
-    const diffScore = (daysDiff + 6) / 15
+  if (env.id !== devEnv.id && devEnv.version) {
+    const devEnvSha = devEnv.version.sha
+    const thisEnvSha = env.version.sha
+    const daysDiff = getDaysDiff(env, devEnv)
 
-    const hue = Math.max(0, (1 - diffScore) * 120).toString(10)
+    $(`#${env.id}_indicator`).css('background-color', getIndicatorColour(daysDiff))
 
-    const style = `hsl(${hue},50%,50%)`
-    $(`#${env}_details`).css('background-color', style)
-
-    const gitDiffUrl = `https://github.com/ministryofjustice/${componentName}/compare/${thisEnvSha}...${devEnvSha}`
+    const gitDiffUrl = `https://github.com/ministryofjustice/${env.componentName}/compare/${thisEnvSha}...${devEnvSha}`
 
     if (devEnvSha !== thisEnvSha) {
-      $(`#${env}_details`).html(`
-        ${tileLine('Environment', env)}
-        ${tileLine('Build date', date)}
-        ${tileLine('Build number', build)}
-        ${tileLine('Staleness', `${daysDiff} days behind dev`)}
-        ${tileLine('Differences', tileLink('View in GitHub', gitDiffUrl))}
+      const diffDescription =
+        daysDiff > 0 ? `${daysDiff} day${daysDiff !== 1 ? 's' : ''} behind dev` : 'A newer build exists'
+
+      $(`#${env.id}_name`).html(tileLine(env.name, diffDescription))
+      $(`#${env.id}_details`).html(`
+        ${tileLine('Version', `${env.version.full}`)}
+        ${tileLink(
+          env.name === prodEnv.name ? 'View unreleased changes in Github' : 'View difference to dev in GitHub',
+          gitDiffUrl,
+        )}
     `)
     }
+  }
+  if (env.id === devEnv.id) {
+    const daysDiff = dayjs(new Date()).diff(env.version.date, 'day')
+    if (daysDiff > 20) {
+      $(`#${env.id}_indicator`).css('background-color', getIndicatorColour(daysDiff))
+    }
+    const diffDescription = `Last build ${Math.max(0, daysDiff)} day${daysDiff != 1 ? 's' : ''} ago`
+    $(`#${devEnv.id}_name`).html(tileLine(devEnv.name, diffDescription))
   }
 }
 
 const fetchMessages = async queryStringOptions => {
   const queryString = new URLSearchParams(queryStringOptions).toString()
-  const response = await fetch(`/drift-radiator/queue/${componentName}/${queryString}`)
+  const csrfToken = $('#csrf').val()
+  const response = await fetch('/drift-radiator/queue', {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'X-CSRF-Token': csrfToken,
+    },
+    body: JSON.stringify({ streams: lastIds }),
+  })
 
   if (!response.ok) {
     throw new Error('There was a problem fetching the component data')
@@ -57,19 +79,20 @@ const fetchMessages = async queryStringOptions => {
   try {
     const streamJson = await response.json()
 
-    streamJson.forEach(stream => {
-      const streamName = stream.name.split(':')
-      const streamType = streamName[0].charAt(0)
-      const streamKey = `${streamType}:${streamName[2]}`
-      const env = streamName[2]
+    streamJson.forEach(({ name, messages }) => {
+      const streamName = name.split(':')
+      const {
+        id,
+        message: { v: version },
+      } = messages[messages.length - 1]
+      lastIds[streamName] = id
 
-      const lastMessage = stream.messages[stream.messages.length - 1]
-      if (lastIds[streamKey]) {
-        lastIds[streamKey] = lastMessage.id
-      }
+      const [date, build, sha] = version.split('.')
+      const envId = streamName.slice(streamName.length - 2).join('_')
+      const thisEnv = envs[envId]
+      thisEnv.version = { full: version, date, build, sha }
 
-      const version = lastMessage.message.v
-      updateVersion({ env, version })
+      updateVersion(thisEnv, envs[thisEnv.devEnvId], envs[thisEnv.prodEnvId])
     })
   } catch (e) {
     console.error(e)
