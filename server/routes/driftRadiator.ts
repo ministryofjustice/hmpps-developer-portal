cdb34d8e93cee00b1f949f9a02916fef0def2950
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2023-12-22 10:07:52 +0000

    Hooking up the drift radiator (#116)
    This adds the drift radiator to the component page and the index page.
    It should make it easier to add filters as the only interface now to
    render the component is a list of services to display the radiator for

--- server/routes/driftRadiator.ts
+++ server/routes/driftRadiator.ts
@@ -1,8 +1,7 @@
 import { type RequestHandler, Router } from 'express'
 import asyncMiddleware from '../middleware/asyncMiddleware'
 import type { Services } from '../services'
-import { getComponentName } from '../utils/utils'
-import type { Component, Environment } from '../data/strapiApiTypes'
+import type { Component } from '../data/strapiApiTypes'
 
 export default function routes({ serviceCatalogueService, redisService }: Services): Router {
   const router = Router()
@@ -10,26 +9,13 @@ export default function routes({ serviceCatalogueService, redisService }: Servic
   const get = (path: string, handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
   const post = (path: string, handler: RequestHandler) => router.post(path, asyncMiddleware(handler))
 
-  const toEnv = (component: Component, env: Environment, index: number) => ({
-    id: `${component.name}_${env.name}`,
-    name: env.name,
-    componentName: component.name,
-    streamName: `version:${component.name}:${env.name}`,
-    devEnvId: `${component.name}_${component.environments[0].name}`,
-    prodEnvId: `${component.name}_${component.environments.at(-1).name}`,
-    isDev: index === 0,
-  })
-
   get('/', async (req, res) => {
     const rawComponents = await serviceCatalogueService.getComponents()
 
     const components = rawComponents
       .map(({ attributes: component }) => component)
       .filter(component => component.environments?.length)
-      .map(component => ({
-        name: component.name,
-        environments: component.environments.map((env, i) => toEnv(component, env, i)),
-      }))
+      .map(component => component.name)
 
     return res.render('pages/driftRadiator', { components })
   })
@@ -40,25 +26,34 @@ export default function routes({ serviceCatalogueService, redisService }: Servic
     return res.send(components)
   })
 
-  get('/:componentName', async (req, res) => {
-    const componentName = getComponentName(req)
-    const component = await serviceCatalogueService.getComponent(componentName)
+  const toComponentView = (component: Component) => ({
+    name: component.name,
+    environments: component.environments.map((env, i) => ({
+      id: `${component.name}_${env.name}`,
+      name: env.name,
+      componentName: component.name,
+      streamName: `version:${component.name}:${env.name}`,
+      devEnvId: `${component.name}_${component.environments[0].name}`,
+      prodEnvId: `${component.name}_${component.environments.at(-1).name}`,
+      isDev: i === 0,
+    })),
+  })
 
-    const displayComponent = {
-      name: component.name,
-      environments: component.environments.map((env, i) => toEnv(component, env, i)),
-    }
+  post('/components.json', async (req, res) => {
+    const { componentNames } = req.body as Record<string, string[]>
 
-    return res.render('pages/driftRadiator', { components: [displayComponent] })
+    const allComponents = await serviceCatalogueService.getComponents()
+    const components = allComponents
+      .filter(c => componentNames.includes(c.attributes.name))
+      .map(c => toComponentView(c.attributes))
+    return res.send(components)
   })
 
   post('/queue', async (req, res) => {
-    const streams = Object.keys(req.body?.streams).map(queueName => {
-      return {
-        key: queueName,
-        id: req.body?.streams[queueName],
-      }
-    })
+    const streams = Object.keys(req.body?.streams).map(queueName => ({
+      key: queueName,
+      id: req.body?.streams[queueName],
+    }))
 
     const messages = await redisService.readStream(streams)
 
