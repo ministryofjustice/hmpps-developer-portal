6ad959c4fb2aa84a875ef851f960a3caad7d3577
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2024-06-17 19:01:50 +0100

    Adding esbuild
    wip - everything broken

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+import * as govukFrontend from 'govuk-frontend'
+import * as mojFrontend from '@ministryofjustice/frontend'
+import * as componentsTable from './componentsTable'
+import * as dependenciesTable from './dependenciesTable'
+import * as driftRadiator from './driftRadiator'
+import * as monitor from './monitor'
+
+govukFrontend.initAll()
+mojFrontend.initAll()
