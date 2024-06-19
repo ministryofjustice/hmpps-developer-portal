f2bf7092cde6ae985124742746686d13dc9a1fbf
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2024-06-19 16:10:29 +0100

    Major JavaScript tody

---
+++ assets/js/productSets.js
@@ -0,0 +1,18 @@
+jQuery(function () {
+  const columns = [
+    {
+      data: 'attributes.ps_id',
+      createdCell: function (td, _cellData, rowData) {
+        $(td).html(`<a href="/product-sets/${rowData.id}">${rowData.attributes.ps_id}</a>`)
+      },
+    },
+    {
+      data: 'attributes.name',
+      createdCell: function (td, _cellData, rowData) {
+        $(td).html(`<a href="/product-sets/${rowData.id}">${rowData.attributes.name}</a>`)
+      },
+    },
+  ]
+
+  createTable('productSetsTable', '/product-sets/data', 1, columns)
+})
---
+++ assets/js/products.js
@@ -0,0 +1,33 @@
+jQuery(function () {
+  const columns = [
+    {
+      data: 'attributes.p_id',
+      createdCell: function (td, _cellData, rowData) {
+        $(td).html(`<a href="/products/${rowData.id}">${rowData.attributes.p_id}</a>`)
+      },
+    },
+    { data: 'attributes.name', render: cleanColumnOutput },
+    {
+      data: 'attributes.product_set.data.attributes.ps_id',
+      createdCell: function (td, _cellData, rowData) {
+        const link = rowData.attributes.product_set.data
+          ? `<a href="/product-sets/${rowData.attributes.product_set.data.id}">${cleanColumnOutput(
+              rowData.attributes.product_set.data.attributes.ps_id,
+            )}</a>`
+          : 'N/A'
+        $(td).html(link)
+      },
+    },
+    {
+      data: 'attributes.product_set.data.attributes.name',
+      createdCell: function (td, _cellData, rowData) {
+        const link = rowData.attributes.product_set.data
+          ? cleanColumnOutput(rowData.attributes.product_set.data.attributes.name)
+          : 'N/A'
+        $(td).html(link)
+      },
+    },
+  ]
+
+  createTable('productsTable', '/products/data', 1, columns)
+})
