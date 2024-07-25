bcc45a66cd7d0fac84eb4c20f90e023215d59770
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2024-07-25 11:39:14 +0100

    update slack links in teams table

--- assets/js/teams.js
+++ assets/js/teams.js
@@ -6,7 +6,17 @@ jQuery(function () {
         $(td).html(`<a href="/teams/${rowData.attributes.slug}">${rowData.attributes.name}</a>`)
       },
     },
+    {
+      data: 'attributes.slack_channel_name',
+      createdCell: function (td, _cellData, rowData) {
+        if (rowData.attributes.slack_channel_id)
+          $(td).html(
+            `<a href="slack://channel?team=T02DYEB3A&id=${rowData.attributes.slack_channel_id}">#${rowData.attributes.slack_channel_name}</a>`,
+          )
+      },
+    },
   ]
+  console.log(columns)
 
   createTable('teamsTable', '/teams/data', 0, 'asc', columns)
 })
