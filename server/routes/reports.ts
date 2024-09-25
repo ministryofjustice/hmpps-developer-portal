a35bbc4ef6d64ba86eed23af437ad749e17e8245
sandhya1874 <sandhya.gandalwar@digital.justice.gov.uk>
2024-09-25 14:27:59 +0100

    Partial working code for Trivy

--- server/routes/reports.ts
+++ server/routes/reports.ts
@@ -71,7 +71,7 @@ export default function routes({ serviceCatalogueService }: Services): Router {
 
   post('/trivy/data', async (req, res) => {
     const componentsToInclude = req.body.componentNames
-    console.log(componentsToInclude)
+    // console.log(componentsToInclude)
 
     const components = (await serviceCatalogueService.getComponents())
       .filter(component => componentsToInclude.includes(component.attributes.name))
--- server/routes/trivy.ts
+++ server/routes/trivy.ts
@@ -3,11 +3,11 @@ import asyncMiddleware from '../middleware/asyncMiddleware'
 import type { Services } from '../services'
 import { isValidDropDown } from '../utils/utils'
 
-export default function routes({ teamHealthService, componentNameService, dataFilterService }: Services): Router {
+export default function routes({ componentNameService, dataFilterService }: Services): Router {
   const router = Router()
 
   const get = (path: string, handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
-  const post = (path: string, handler: RequestHandler) => router.post(path, asyncMiddleware(handler))
+  // const post = (path: string, handler: RequestHandler) => router.post(path, asyncMiddleware(handler))
 
   get('/', async (req, res) => {
     if (req.query.updateServiceArea === '' && isValidDropDown(req, 'serviceArea')) {
