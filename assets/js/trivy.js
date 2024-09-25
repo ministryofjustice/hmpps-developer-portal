70cabbad1cfcf321e1f4b2ff3a8ed11bf7318672
sandhya1874 <sandhya.gandalwar@digital.justice.gov.uk>
2024-09-25 15:21:17 +0100

    .

--- assets/js/trivy.js
+++ assets/js/trivy.js
@@ -65,7 +65,6 @@ jQuery(function () {
     const dropDownText = $(`#${dropDownType} option:selected`).text()
     const dropDownTypeIdValue = Number.parseInt($(`#${dropDownType}`).val())
     const dropDownTypeId = Number.isNaN(dropDownTypeIdValue) ? 0 : dropDownTypeIdValue
-    const response = await fetch(`/trivy/${dropDownType}/${formatTrivyName(dropDownText)}`)
     window.location = `/trivy/${dropDownType}/${formatTrivyName(dropDownText)}`
   })
 })
