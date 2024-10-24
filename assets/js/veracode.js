dafe603b6d1a56f287d457d4094a20870c03461c
sandhya1874 <sandhya.gandalwar@digital.justice.gov.uk>
2024-10-24 20:15:26 +0100

    Updated to create table before adding new filter

--- assets/js/veracode.js
+++ assets/js/veracode.js
@@ -1,76 +1,5 @@
 jQuery(function () {
   const rootDataUrl = '/reports/veracode/data'
-  const columns = [
-    {
-      data: 'name',
-      createdCell: function (td, _cellData, rowData) {
-        $(td).html(`<a href="/components/${cleanColumnOutput(rowData.name)}">${cleanColumnOutput(rowData.name)}</a>`)
-      },
-    },
-    {
-      data: 'result',
-      createdCell: function (td, _cellData, rowData) {
-        let className = 'veracode--missing'
-        let data = 'N/A'
-
-        if (rowData.hasVeracode) {
-          className = rowData.result === 'Passed' ? 'veracode--passed' : 'veracode--failed'
-          data = rowData.result
-        }
-
-        $(td).html(data).addClass(className)
-      },
-    },
-    {
-      data: 'date',
-      createdCell: function (td, _cellData, rowData) {
-        $(td).html(`${rowData.date}`).addClass('td-nowrap')
-      },
-    },
-    {
-      data: 'severityLevels.VERY_HIGH',
-      createdCell: function (td, _cellData, rowData) {
-        const data = rowData.hasVeracode ? rowData.severityLevels.VERY_HIGH : 'N/A'
-        $(td).html(data)
-      },
-    },
-    {
-      data: 'severityLevels.HIGH',
-      createdCell: function (td, _cellData, rowData) {
-        const data = rowData.hasVeracode ? rowData.severityLevels.HIGH : 'N/A'
-        $(td).html(data)
-      },
-    },
-    {
-      data: 'severityLevels.MEDIUM',
-      createdCell: function (td, _cellData, rowData) {
-        const data = rowData.hasVeracode ? rowData.severityLevels.MEDIUM : 'N/A'
-        $(td).html(data)
-      },
-    },
-    {
-      data: 'severityLevels.LOW',
-      createdCell: function (td, _cellData, rowData) {
-        const data = rowData.hasVeracode ? rowData.severityLevels.LOW : 'N/A'
-        $(td).html(data)
-      },
-    },
-    {
-      data: 'codeScore',
-      createdCell: function (td, _cellData, rowData) {
-        const data = rowData.hasVeracode ? rowData.codeScore : 0
-        $(td).html(data)
-      },
-      type: 'num',
-    },
-    {
-      data: 'report',
-      createdCell: function (td, _cellData, rowData) {
-        const data = rowData.hasVeracode ? `<a href="${rowData.report}" target="_blank">View</a>` : 'N/A'
-        $(td).html(data)
-      },
-    },
-  ]
 
   const veracodeTable = createTable({
     id: 'veracodeTable',
@@ -79,7 +8,7 @@ jQuery(function () {
     orderType: 'desc',
     columns,
   })
-
+  console.log(veracodeTable)
   $('#updateVeracodeFilters').on('click', async e => {
     e.preventDefault(e)
     const selectedResultFilters = []
@@ -98,3 +27,75 @@ jQuery(function () {
     veracodeTable.ajax.url(newDataUrl).load()
   })
 })
+
+const columns = [
+  {
+    data: 'name',
+    createdCell: function (td, _cellData, rowData) {
+      $(td).html(`<a href="/components/${cleanColumnOutput(rowData.name)}">${cleanColumnOutput(rowData.name)}</a>`)
+    },
+  },
+  {
+    data: 'result',
+    createdCell: function (td, _cellData, rowData) {
+      let className = 'veracode--missing'
+      let data = 'N/A'
+
+      if (rowData.hasVeracode) {
+        className = rowData.result === 'Passed' ? 'veracode--passed' : 'veracode--failed'
+        data = rowData.result
+      }
+
+      $(td).html(data).addClass(className)
+    },
+  },
+  {
+    data: 'date',
+    createdCell: function (td, _cellData, rowData) {
+      $(td).html(`${rowData.date}`).addClass('td-nowrap')
+    },
+  },
+  {
+    data: 'severityLevels.VERY_HIGH',
+    createdCell: function (td, _cellData, rowData) {
+      const data = rowData.hasVeracode ? rowData.severityLevels.VERY_HIGH : 'N/A'
+      $(td).html(data)
+    },
+  },
+  {
+    data: 'severityLevels.HIGH',
+    createdCell: function (td, _cellData, rowData) {
+      const data = rowData.hasVeracode ? rowData.severityLevels.HIGH : 'N/A'
+      $(td).html(data)
+    },
+  },
+  {
+    data: 'severityLevels.MEDIUM',
+    createdCell: function (td, _cellData, rowData) {
+      const data = rowData.hasVeracode ? rowData.severityLevels.MEDIUM : 'N/A'
+      $(td).html(data)
+    },
+  },
+  {
+    data: 'severityLevels.LOW',
+    createdCell: function (td, _cellData, rowData) {
+      const data = rowData.hasVeracode ? rowData.severityLevels.LOW : 'N/A'
+      $(td).html(data)
+    },
+  },
+  {
+    data: 'codeScore',
+    createdCell: function (td, _cellData, rowData) {
+      const data = rowData.hasVeracode ? rowData.codeScore : 0
+      $(td).html(data)
+    },
+    type: 'num',
+  },
+  {
+    data: 'report',
+    createdCell: function (td, _cellData, rowData) {
+      const data = rowData.hasVeracode ? `<a href="${rowData.report}" target="_blank">View</a>` : 'N/A'
+      $(td).html(data)
+    },
+  },
+]
