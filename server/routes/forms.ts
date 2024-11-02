de833bd9e9f99c11cb92719bec923cabe3fe9af6
sandhya1874 <sandhya.gandalwar@digital.justice.gov.uk>
2024-11-02 17:25:20 +0000

    Resolved lint failure issues

--- server/routes/forms.ts
+++ server/routes/forms.ts
@@ -2,13 +2,43 @@ import { type RequestHandler, Router } from 'express'
 import asyncMiddleware from '../middleware/asyncMiddleware'
 import type { Services } from '../services'
 
-export default function routes({ serviceCatalogueService, componentNameService, dataFilterService }: Services): Router {
+export default function routes({ componentNameService, dataFilterService }: Services): Router {
   const router = Router()
 
   const get = (path: string, handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
   get('/', async (req, res) => {
-    return res.render('pages/componentrequest', {})
+    const components = await componentNameService.getAllDeployedComponents()
+    const [productList] = await dataFilterService.getDropDownLists({
+      productName: '',
+      useFormattedName: true,
+    })
+
+    return res.render('pages/componentrequest', {
+      title: 'Github repository Name',
+      productList,
+    })
+  })
+
+  get('/products/:productName', async (req, res) => {
+    const { productName } = req.params
+    const components = await componentNameService.getAllDeployedComponentsForProduct(productName)
+    const [teamList, productList, serviceAreaList, customComponentsList] = await dataFilterService.getDropDownLists({
+      teamName: '',
+      productName,
+      serviceAreaName: '',
+      customComponentName: '',
+      useFormattedName: true,
+    })
+
+    return res.render('pages/componentrequest', {
+      title: `Github repository Name for ${productName}`,
+      components,
+      serviceAreaList,
+      teamList,
+      productList,
+      customComponentsList,
+    })
   })
 
   return router
