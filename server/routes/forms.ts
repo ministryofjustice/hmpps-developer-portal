52163555a928a03a66907b4e68de3939b7c9433a
sandhya1874 <sandhya.gandalwar@digital.justice.gov.uk>
2024-11-13 15:48:54 +0000

    Updated to address the comments

--- server/routes/forms.ts
+++ server/routes/forms.ts
@@ -11,32 +11,12 @@ export default function routes({ dataFilterService }: Services): Router {
   const get = (path: string, handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
   const post = (path: string, handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
-  get('/', async (req, res) => {
-    const [productList] = await dataFilterService.getDropDownLists({
-      productName: '',
-      useFormattedName: true,
-    })
-
-    return res.render('pages/githubRepoRequestForm', {
-      title: 'Github repository Name',
-      productList,
-    })
+  get('/github-repo-request-form', async (req, res) => {
+    console.log(`in get`)
+    return res.render('pages/githubRepoRequestForm')
   })
 
-  get('/products/:productName', async (req, res) => {
-    const { productName } = req.params
-    const [productList] = await dataFilterService.getDropDownLists({
-      productName,
-      useFormattedName: true,
-    })
-
-    return res.render('pages/githubRepoRequestForm', {
-      title: `Github repository Name for ${productName}`,
-      productList,
-    })
-  })
-
-  post('/new-github-repo-request-form', async (req, res): Promise<void> => {
+  post('/github-repo-request-form', async (req, res): Promise<void> => {
     // const (github_repo,repo_description,base_template,github_project_visibility,jira_project_keys,product,github_projects_teams_admin,github_project_teams_writ,github_project_branch_protection_restricted_teams,slack_channel_release_notify,slack_channel_pipeline_notify,nonprod_alerts_severity_label,prod_alerts_severity_label) = req.body;
     const { github_repo } = req.body.github_repo
     // const { repo_description } = req.body.repo_description
@@ -53,7 +33,9 @@ export default function routes({ dataFilterService }: Services): Router {
     // const prod_alerts_severity_label = req.body.nonprod_alerts_severity_label
     console.log(`in post `)
     console.log(github_repo)
-    return res.redirect(`pages/githubRepoRequestForm`)
+    return res.render('pages/githubRepoRequestForm', {
+      title: 'Github repository Name',
+    })
   })
 
 
