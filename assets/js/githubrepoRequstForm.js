b88b729086e53782e239978ad0dc16df9af937d0
sandhya1874 <sandhya.gandalwar@digital.justice.gov.uk>
2024-11-13 12:14:35 +0000

    Removed Javascript and kept one field for testing in form

--- assets/js/githubrepoRequstForm.js
+++ assets/js/githubrepoRequstForm.js
@@ -3,17 +3,5 @@ jQuery(function () {
     e.preventDefault(e)
     console.log(`submit clicked`)
 
-    $.ajax({
-      url: 'https://test.co.uk/v1/sss',
-      crossDomain: true,
-      method: 'post',
-      headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer 25ccee26685f02dd6da43f63',
-      },
-      contentType: 'application/json',
-    }).done(function (response) {
-      console.log(response)
-    })
   })
 })
