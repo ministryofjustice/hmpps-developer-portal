6b31ed6516516ec051cf1ddeb4614d37032346fd
sandhya1874 <sandhya.gandalwar@digital.justice.gov.uk>
2024-11-20 14:37:17 +0000

    Working validation with linter error

---
+++ server/routes/formValidation.ts
@@ -0,0 +1,88 @@
+export enum ValidationType {
+  REQUIRED,
+  MIN,
+  MAX,
+  TRUE,
+  NAMEVALIDATE,
+}
+
+interface ValidationTypeMinProps {
+  min: number
+}
+
+interface ValidationTypeMaxProps {
+  max: number
+}
+
+type ValidatorProps = ValidationTypeMinProps | ValidationTypeMaxProps
+
+export type Validator = ValidatorInterface<ValidatorProps>
+
+export interface ValidatorInterface<ValidatorTypes> {
+  validatorType: ValidationType
+  message: string
+  props?: ValidatorProps
+}
+
+export interface ValidationResponse {
+  valid: boolean
+  errors: Validator[]
+}
+
+export const validate = (value: any, validators: Validator[]): ValidationResponse => {
+  const response: ValidationResponse = {
+    valid: true,
+    errors: [],
+  }
+  const namePattern = /^[a-zA-Z0-9-]+$/
+  validators.forEach((validator: Validator) => {
+    switch (validator.validatorType) {
+      case ValidationType.REQUIRED:
+        if (value === undefined || value === null || value.trim() === '') {
+          response.errors.push(validator)
+          response.valid = false
+        }
+        break
+      case ValidationType.MIN:
+        if (typeof value === 'number' && validator.props && 'min' in validator.props) {
+          if (value < validator.props.min) {
+            response.errors.push(validator)
+            response.valid = false
+          }
+        }
+        break
+      case ValidationType.MAX:
+        if (typeof value === 'number' && validator.props && 'max' in validator.props) {
+          if (value > validator.props.max) {
+            response.errors.push(validator)
+            response.valid = false
+          }
+        }
+        break
+      case ValidationType.NAMEVALIDATE:
+        if (typeof value === 'string') {
+          if (!namePattern.test(value)) {
+            response.errors.push(validator)
+            response.valid = false
+          }
+        }
+        break
+    }
+  })
+  return response
+}
+
+export const validateFormFields = (
+  formValues: { [key: string]: any },
+  formValidators: { [key: string]: Validator[] },
+): { [key: string]: ValidationResponse } => {
+  const validationResults: { [key: string]: ValidationResponse } = {}
+
+  for (const field in formValues) {
+    if (formValidators[field]) {
+      validationResults[field] = validate(formValues[field], formValidators[field])
+    }
+  }
+
+  return validationResults
+}
--- server/routes/forms.ts
+++ server/routes/forms.ts
@@ -2,6 +2,7 @@ import { type RequestHandler, Router } from 'express'
 import asyncMiddleware from '../middleware/asyncMiddleware'
 import type { Services } from '../services'
 import { GithubRepoRequestRequest } from '../data/strapiApiTypes'
+import * as Validation from './formValidation'
 
 export default function routes({ serviceCatalogueService, dataFilterService }: Services): Router {
   const router = Router()
@@ -22,11 +23,41 @@ export default function routes({ serviceCatalogueService, dataFilterService }: S
 
   post('/github-repo-request-form', async (req, res): Promise<void> => {
     const formData = req.body
+    const formValidators: { [key: string]: Validation.Validator[] } = {
+      github_repo: [
+        {
+          validatorType: Validation.ValidationType.NAMEVALIDATE,
+          message: 'Repository Name should be Alphanumeric and only allowed special character is - (hyphen)',
+        },
+      ],
+      // Uncomment and add other validators as needed
+      // github_description: [
+      //   { validatorType: Validation.ValidationType.REQUIRED, message: 'Enter Github Repository Description' }
+      // ],
+      // alerts_prod_slack_channel: [
+      //   { validatorType: Validation.ValidationType.REQUIRED, message: 'Production Slack channel is required' }
+      // ],
+      // alerts_nonprod_slack_channel: [
+      //     { validatorType: Validation.ValidationType.REQUIRED, message: 'Non-production Slack channel is required' }
+      // ]
+    }
+    const validationResults = Validation.validateFormFields(formData, formValidators)
+    console.log('formData', formData)
+    console.log('validationResults', validationResults)
+    const hasErrors = Object.values(validationResults).some(result => !result.valid)
+    console.log('hasErrors', hasErrors)
+    if (hasErrors) {
+      return res.render('pages/githubRepoRequestForm', {
+        formData,
+        validationResults,
+      })
+    }
+
     const requestFormData = buildFormData(formData)
     await serviceCatalogueService.postGithubRepoRequest(requestFormData)
 
     // disable submit page, give confirmation and stay on page or redirect to new page with summary of requested data pushed to strapi
-    return res.redirect('/forms/github-repo-request-form')
+    // return res.redirect('/forms/github-repo-request-form')
   })
 
   return router
@@ -36,22 +67,22 @@ export const buildFormData = (formData: Record<string, unknown>): GithubRepoRequ
   return {
     data: {
       github_repo: formData.github_repo?.toString(),
-      repo_description: formData.repo_description?.toString(),
-      base_template: formData.base_template?.toString(),
-      jira_project_keys: formData.jira_project_keys?.toString(),
-      product: formData.product?.toString(),
-      github_project_visibility: JSON.parse(formData.github_project_visibility.toString()),
-      github_project_teams_write: formData.github_project_teams_write?.toString().split(','),
-      github_projects_teams_admin: formData.github_projects_teams_admin?.toString().split(','),
-      github_project_branch_protection_restricted_teams: formData.github_project_branch_protection_restricted_teams
-        ?.toString()
-        .split(','),
-      slack_channel_nonprod_release_notify: formData.slack_channel_nonprod_release_notify?.toString(),
-      slack_channel_pipeline_notify: formData.slack_channel_pipeline_notify?.toString(),
-      prod_alerts_severity_label: formData.prod_alerts_severity_label?.toString(),
-      nonprod_alerts_severity_label: formData.nonprod_alerts_severity_label?.toString(),
-      alerts_prod_slack_channel: formData.alerts_prod_slack_channel?.toString(),
-      alerts_nonprod_slack_channel: formData.alerts_nonprod_slack_channel?.toString(),
+      // repo_description: formData.repo_description?.toString(),
+      // base_template: formData.base_template?.toString(),
+      // jira_project_keys: formData.jira_project_keys?.toString(),
+      // product: formData.product?.toString(),
+      // github_project_visibility: JSON.parse(formData.github_project_visibility.toString()),
+      // github_project_teams_write: formData.github_project_teams_write?.toString().split(','),
+      // github_projects_teams_admin: formData.github_projects_teams_admin?.toString().split(','),
+      // github_project_branch_protection_restricted_teams: formData.github_project_branch_protection_restricted_teams
+      //   ?.toString()
+      //   .split(','),
+      // slack_channel_nonprod_release_notify: formData.slack_channel_nonprod_release_notify?.toString(),
+      // slack_channel_pipeline_notify: formData.slack_channel_pipeline_notify?.toString(),
+      // prod_alerts_severity_label: formData.prod_alerts_severity_label?.toString(),
+      // nonprod_alerts_severity_label: formData.nonprod_alerts_severity_label?.toString(),
+      // alerts_prod_slack_channel: formData.alerts_prod_slack_channel?.toString(),
+      // alerts_nonprod_slack_channel: formData.alerts_nonprod_slack_channel?.toString(),
     },
   }
 }
