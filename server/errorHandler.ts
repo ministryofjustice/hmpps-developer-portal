431e4e92068c6ac9a3affb60ca667fb2abb4e28d
sandhya1874 <sandhya.gandalwar@digital.justice.gov.uk>
2024-11-22 13:55:39 +0000

    Working validation part with staying on the same page and showing errors

--- server/errorHandler.ts
+++ server/errorHandler.ts
@@ -1,10 +1,16 @@
 import type { Request, Response, NextFunction } from 'express'
 import type { HTTPError } from 'superagent'
 import logger from '../logger'
+import { ValidationError } from './middleware/setUpValidationMiddleware'
 
 export default function createErrorHandler(production: boolean) {
   return (error: HTTPError, req: Request, res: Response, next: NextFunction): void => {
     logger.error(`Error handling request for '${req.originalUrl}', user '${res.locals.user?.username}'`, error)
+
+    if (error instanceof ValidationError) {
+      logger.info(`validation error on: ${req.originalUrl}`)
+      return res.redirect(req.originalUrl)
+    }
 
     res.locals.message = production
       ? 'Something went wrong. The error has been logged. Please try again'
