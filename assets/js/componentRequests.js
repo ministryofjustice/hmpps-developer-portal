5fb4f69dd7d56d03910d3851463b8b1199f2e6dc
sandhya1874 <sandhya.gandalwar@digital.justice.gov.uk>
2024-11-27 14:54:22 +0000

    Added all details in componentrequests and single request page

--- assets/js/componentRequests.js
+++ assets/js/componentRequests.js
@@ -1,18 +1,40 @@
 jQuery(function () {
-    const columns = [
-      {
-        data: 'attributes.github_repo',
-        createdCell: function (td, _cellData, rowData) {
-          $(td).html(`<a href="/forms/component-requests/${rowData.attributes.github_repo}">${rowData.attributes.github_repo}</a>`)
-        },
+  const columns = [
+    {
+      data: 'attributes.github_repo',
+      createdCell: function (td, _cellData, rowData) {
+        $(td).html(
+          `<a href="/forms/component-requests/${rowData.attributes.github_repo}">${rowData.attributes.github_repo}</a>`,
+        )
       },
-      {
-        data: 'attributes.repo_description',
-        createdCell: function (td, _cellData, rowData) {
-          $(td).html(`${rowData.attributes.repo_description}</a>`)
-        },
+    },
+    {
+      data: 'attributes.requester_name',
+      createdCell: function (td, _cellData, rowData) {
+        $(td).html(`${rowData.attributes.requester_name}`)
       },
-    ]
+    },
+    {
+      data: 'attributes.request_github_pr_status',
+      createdCell: function (td, _cellData, rowData) {
+        $(td).html(`${rowData.attributes.request_github_pr_status}`)
+      },
+    },
+    {
+      data: 'attributes.request_github_pr_number',
+      createdCell: function (td, _cellData, rowData) {
+        $(td).html(
+          `<a href="https://github.com/ministryofjustice/hmpps-project-bootstrap/pull/${rowData.attributes.request_github_pr_number}">${rowData.attributes.request_github_pr_number}</a>`,
+        )
+      },
+    },
+  ]
 
-    createTable({ id: 'componentRequestsTable', ajaxUrl: '/forms/component-requests/data', orderColumn: 0, orderType: 'asc', columns })
+  createTable({
+    id: 'componentRequestsTable',
+    ajaxUrl: '/forms/component-requests/data',
+    orderColumn: 0,
+    orderType: 'asc',
+    columns,
   })
+})
