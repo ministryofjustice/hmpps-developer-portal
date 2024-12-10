ad384d8c3ff796d27d2bc44ba3c9b2d7bfb93b0d
sandhya1874 <sandhya.gandalwar@digital.justice.gov.uk>
2024-12-10 16:19:03 +0000

    Added regexp search at the footer

--- assets/js/components.js
+++ assets/js/components.js
@@ -69,6 +69,10 @@ jQuery(function () {
   ]
 
   $(function () {
+    $('#componentsTable tfoot th').each(function () {
+      var title = $(this).text()
+      $(this).html('<input type="text" placeholder="Search ' + title + '" />')
+    })
     var table = new DataTable('#componentsTable', {
       ajax: {
         url: '/components/data',
@@ -91,6 +95,15 @@ jQuery(function () {
         // topStart: 'paging',
         topEnd: 'search',
       },
+    })
+    table.columns().every(function () {
+      var that = this
+
+      $('input', this.footer()).on('keyup change', function () {
+        if (that.search() !== this.value) {
+          that.search(this.value, true, false).draw()
+        }
+      })
     })
   })
 })
