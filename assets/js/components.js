e9631f16585c1f22cb3d9997d24c28c31534be8f
sandhya1874 <sandhya.gandalwar@digital.justice.gov.uk>
2024-12-10 16:13:56 +0000

    Replaced createTable with DataTable

--- assets/js/components.js
+++ assets/js/components.js
@@ -1,79 +1,96 @@
 jQuery(function () {
   const columns = [
     {
+      targets: 0,
       data: 'attributes.name',
-      createdCell: function (td, _cellData, rowData) {
-        $(td).html(
-          `<a class="govuk-link--no-visited-state" href="/components/${cleanColumnOutput(rowData.attributes.name)}">${cleanColumnOutput(
-            rowData.attributes.name,
-          )}</a>`,
-        )
+      render: function (data, type, row, meta) {
+        return `<a class="govuk-link--no-visited-state" href="/components/${cleanColumnOutput(data)}">${cleanColumnOutput(data)}</a>`
       },
     },
     {
+      targets: 1,
       data: 'attributes.product.data.attributes.p_id',
-      createdCell: function (td, _cellData, rowData) {
-        const link = rowData.attributes.product.data
-          ? `<a class="govuk-link--no-visited-state" href="/products/${rowData.attributes.product.data.attributes.slug}">${cleanColumnOutput(
-              rowData.attributes.product.data.attributes.p_id,
-            )}</a>`
-          : 'N/A'
-        $(td).html(link)
+      render: function (data, type, row, meta) {
+        if (row.attributes.product.data) {
+          return `<a class="govuk-link--no-visited-state" href="/products/${row.attributes.product.data.attributes.slug}">${cleanColumnOutput(data)}</a>`
+        }
+        return 'N/A'
       },
     },
     {
+      targets: 2,
       data: 'attributes.product.data.attributes.name',
-      createdCell: function (td, _cellData, rowData) {
-        const link = rowData.attributes.product.data
-          ? `<a class="govuk-link--no-visited-state" href="/products/${rowData.attributes.product.data.attributes.slug}">${cleanColumnOutput(
-              rowData.attributes.product.data.attributes.name,
-            )}</a>`
-          : 'N/A'
-        $(td).html(link)
+      render: function (data, type, row, meta) {
+        if (row.attributes.product.data) {
+          return `<a class="govuk-link--no-visited-state" href="/products/${row.attributes.product.data.attributes.slug}">${cleanColumnOutput(data)}</a>`
+        }
+        return 'N/A'
       },
     },
     {
+      targets: 3,
       data: 'attributes.github_repo',
-      createdCell: function (td, _cellData, rowData) {
-        $(td).html(
-          `<a class="govuk-link--no-visited-state" href="https://github.com/ministryofjustice/${rowData.attributes.github_repo}" target="_blank" data-test="github-repo"> ${rowData.attributes.github_repo}</a>`,
-        )
+      render: function (data, type, row, meta) {
+        return `<a class="govuk-link--no-visited-state" href="https://github.com/ministryofjustice/${data}" target="_blank" data-test="github-repo">${data}</a>`
       },
     },
     {
+      targets: 4,
       data: 'attributes.github_enforce_admins_enabled',
       visible: false,
-      createdCell: function (td, _cellData, rowData) {
-        $(td).html(`${rowData.attributes.github_enforce_admins_enabled}`)
+      render: function (data, type, row, meta) {
+        return `${data}`
       },
     },
     {
+      targets: 5,
       data: 'attributes.environments',
-      createdCell: function (td, _cellData, rowData) {
-        const environments = rowData.attributes.environments
+      render: function (data, type, row, meta) {
+        const environments = row.attributes.environments
         const prodEnvironments = environments ? environments.filter(env => env.name === 'prod') : []
 
         let prodSlackChannel = ''
         if (prodEnvironments.length === 0) {
           prodSlackChannel = 'No Prod environment'
         } else {
-          prodSlackChannel = prodEnvironments.map(env => {
-            if (env.alerts_slack_channel === null) {
-              return 'Missing slack channel data'
-            }
-            return `${env.alerts_slack_channel}`
-          })
+          prodSlackChannel = prodEnvironments
+            .map(env => {
+              if (env.alerts_slack_channel === null) {
+                return 'Missing slack channel data'
+              }
+              return `${env.alerts_slack_channel}`
+            })
+            .join(', ') // Join the array elements into a single string
         }
-        $(td).html(prodSlackChannel)
+
+        return prodSlackChannel
       },
     },
   ]
 
-  createTable({
-    id: 'componentsTable',
-    ajaxUrl: '/components/data',
-    orderColumn: 0,
-    orderType: 'asc',
-    columns,
+  $(function () {
+    var table = new DataTable('#componentsTable', {
+      ajax: {
+        url: '/components/data',
+        dataSrc: '',
+        error: function (xhr, error, thrown) {
+          console.error('Error fetching data:', error, thrown)
+          console.error('Response:', xhr.responseText)
+        },
+      },
+      order: [[0, 'asc']],
+      columnDefs: columns,
+      lengthMenu: [
+        [10, 25, 50, -1],
+        [10, 25, 50, 'All'],
+      ],
+      layout: {
+        bottomStart: {
+          buttons: ['colvis', 'copy', 'csv'],
+        },
+        // topStart: 'paging',
+        topEnd: 'search',
+      },
+    })
   })
 })
