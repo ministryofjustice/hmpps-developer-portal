db72fd2e8c2498f908cc3939827ddd4e31bc8b57
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2024-12-18 09:08:45 +0000

    Reading CSRF token from header or form param (#269)

--- server/middleware/setUpCsrf.ts
+++ server/middleware/setUpCsrf.ts
@@ -11,10 +11,9 @@ export default function setUpCsrf(): Router {
     const {
       csrfSynchronisedProtection, // This is the default CSRF protection middleware.
     } = csrfSync({
-      // By default, csrf-sync uses x-csrf-token header, but we use the token in forms and send it in the request body, so change getTokenFromRequest so it grabs from there
       getTokenFromRequest: req => {
         // eslint-disable-next-line no-underscore-dangle
-        return req.body._csrf
+        return req.body._csrf || req.headers['x-csrf-token']
       },
     })
 
