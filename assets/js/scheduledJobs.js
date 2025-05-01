09995f7c3cd803a6528f7610a91f602974524b7b
Sandhya1874 <39799075+Sandhya1874@users.noreply.github.com>
2025-05-01 13:58:19 +0100

    Updated scheduled-jobs reports to show details summary if array not empty (#312)

--- assets/js/scheduledJobs.js
+++ assets/js/scheduledJobs.js
@@ -41,24 +41,21 @@ jQuery(function () {
       createdCell: function (td, _cellData, rowData) {
         const result = rowData.attributes.result
         const errorDetails = rowData.attributes.error_details
-
-        if (result === 'Failed' || result === 'Errors') {
-          if (Array.isArray(errorDetails) && errorDetails.length > 0) {
-            const errorList = errorDetails.map(error => `<li>${error}</li>`).join('')
-            const detailsContent = `<details class="govuk-details">
-              <summary class="govuk-details__summary">
-                <span class="govuk-details__summary-text">
-                  Error Details
-                </span>
-              </summary>
-              <div class="govuk-details__text">
-                <ul>${errorList}</ul>
-              </div>
-            </details>`
-            $(td).html(detailsContent)
-          } else {
-            $(td).html('')
-          }
+        if (Array.isArray(errorDetails) && errorDetails.length > 0) {
+          const errorList = errorDetails.map(error => `<li>${error}</li>`).join('')
+          const detailsContent = `<details class="govuk-details">
+            <summary class="govuk-details__summary">
+              <span class="govuk-details__summary-text">
+                Error Details
+              </span>
+            </summary>
+            <div class="govuk-details__text">
+              <ul>${errorList}</ul>
+            </div>
+          </details>`
+          $(td).html(detailsContent)
+        } else {
+          $(td).html('')
         }
       },
     },
