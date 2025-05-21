eee3c068ee6daaf863e34ee907174dbf5d4fcdcb
Chux <chukwudi.ugorji@digital.justice.gov.uk>
2025-05-21 16:25:25 +0100

    fixed component not loading with error

--- server/routes/components.ts
+++ server/routes/components.ts
@@ -76,7 +76,6 @@ export default function routes({ serviceCatalogueService, redisService }: Servic
     let alerts: DisplayAlert[] = []
     try {
       const url = `${config.apis.alertManager.url}/alerts?filter=application="${componentName}"`
-      // Temporarily disabling the fetch call to alert manager
       const resp = await fetch(url)
       const parsed = await resp.json()
       const dataArray = Array.isArray(parsed) ? (parsed as Alert[]) : []
