164f79b69663a0d5fbadcdccb66f70b2c4e39394
Eve Bramley <eve.bramley@digital.justice.gov.uk>
2025-06-19 11:16:28 +0100

    updated timestamp function to Sandhya's method on Trivy page

--- assets/js/alerts.js
+++ assets/js/alerts.js
@@ -177,17 +177,21 @@ function getFiltersFromURL() {
   }
 }
 
-function formatTimeStamp(date) {
-  const day = date.getUTCDate()
-  const monthNames = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
-  const month = monthNames[date.getUTCMonth()]
-  const year = date.getUTCFullYear()
-
-  const hours = String(date.getUTCHours()).padStart(2, '0')
-  const minutes = String(date.getUTCMinutes()).padStart(2, '0')
-  const seconds = String(date.getUTCSeconds()).padStart(2, '0')
-
-  return `${day} ${month} ${year} ${hours}:${minutes}:${seconds}`
+function formatTimeStamp(dateString) {
+  if (!dateString) return 'N/A'
+  const date = new Date(dateString)
+  return date
+    .toLocaleString('en-GB', {
+      day: '2-digit',
+      month: 'short',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false,
+    })
+    .replace(',', '')
+    .toUpperCase()
 }
 
 //  append tabledata to the #alertsStatusTable
