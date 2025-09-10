94fb8898a86070f4e3a5228c18f5b8eef9be59dc
Eve Bramley <eve.bramley@digital.justice.gov.uk>
2025-09-10 17:49:05 +0100

    removed unrelated tests

--- server/routes/testutils/appSetup.ts
+++ server/routes/testutils/appSetup.ts
@@ -11,8 +11,6 @@ import serviceAreaRoutes from '../serviceAreas'
 import monitorRoutes from '../monitor'
 import dependencyRoutes from '../dependencies'
 import productDependenciesRoutes from '../productDependencies'
-import missingFromCatalogueRoutes from '../missingFromCatalogue'
-import namespacesRoutes from '../namespaces'
 import componentRequestRoutes from '../componentRequests'
 import scheduledJobsRoutes from '../scheduledJobs'
 import alertsRoutes from '../alerts'
@@ -56,8 +54,6 @@ function appSetup(services: Services, production: boolean): Express {
   app.use('/monitor', monitorRoutes(services))
   app.use('/dependencies', dependencyRoutes(services))
   app.use('/product-dependencies', productDependenciesRoutes(services))
-  app.use('/missing-from-catalogue', missingFromCatalogueRoutes(services))
-  app.use('/namespaces', namespacesRoutes(services))
   app.use('/component-requests', componentRequestRoutes(services))
   app.use('/scheduled-jobs', scheduledJobsRoutes(services))
   app.use('/alerts', alertsRoutes(services))
