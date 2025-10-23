dfd172a37db531852a9347fec0f3be0cee1caae1
Sophie <sophie.metcalf@justice.gov.uk>
2025-10-23 15:32:46 +0100

    removed some duplication to clean up code and moved one variable into route

--- server/routes/components.ts
+++ server/routes/components.ts
@@ -107,6 +107,8 @@ export default function routes({
     }
     displayComponent.alerts = alerts
 
+    const hasAlerts = alerts && alerts.length > 0
+
     let upgradeNeeded = false
 
     const isKotlin = (component.language || '') === 'Kotlin'
@@ -146,7 +148,7 @@ export default function routes({
       }
     }
 
-    return res.render('pages/component', { component: displayComponent, upgradeNeeded })
+    return res.render('pages/component', { component: displayComponent, upgradeNeeded, hasAlerts })
   })
 
   router.get('/:componentName/environment/:environmentName', async (req, res) => {
