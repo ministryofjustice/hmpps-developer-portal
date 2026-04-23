1af937c972f4e8633faebeb01f479561da1370b0
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2026-04-23 16:20:56 +0100

    Update dependencies 2026-04-23

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -5,11 +5,11 @@ export default configureAllowedScripts({
     // Needed by esbuild for watching files during development
     'node_modules/@parcel/watcher@2.5.6': 'ALLOW',
     // Needed by cypress for running tests
-    'node_modules/cypress@15.13.1': 'ALLOW',
+    'node_modules/cypress@^15.14.0': 'ALLOW',
     // Provides native integration, supporting ability to write dtrace probes for bunya
-    'node_modules/dtrace-provider@0.8.8': 'ALLOW',
+    'node_modules/dtrace-provider@^0.8.8': 'FORBID',
     // Needed by jest for running tests in watch mode
-    'node_modules/fsevents@2.3.3': 'ALLOW',
+    'node_modules/fsevents@^2.3.3': 'FORBID',
     // Native solution to quickly resolve module paths, used by jest and eslint
     'node_modules/unrs-resolver@1.11.1': 'ALLOW',
   },
