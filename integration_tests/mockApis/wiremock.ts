3623d2454e67a0105fab167205e5fe9fb7f10413
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2026-07-10 14:36:43 +0100

    Route service catalogue and alertmanager through wiremock proxies

--- integration_tests/mockApis/wiremock.ts
+++ integration_tests/mockApis/wiremock.ts
@@ -8,6 +8,6 @@ const stubFor = (mapping: Record<string, unknown>): SuperAgentRequest =>
 const getMatchingRequests = body => superagent.post(`${url}/requests/find`).send(body)
 
 const resetStubs = (): Promise<Array<Response>> =>
-  Promise.all([superagent.delete(`${url}/mappings`), superagent.delete(`${url}/requests`)])
+  Promise.all([superagent.post(`${url}/mappings/reset`), superagent.delete(`${url}/requests`)])
 
 export { stubFor, getMatchingRequests, resetStubs }
