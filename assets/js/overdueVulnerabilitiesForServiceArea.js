e89a768ab3e615eb60163b3e36e7415071f01452
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2026-07-14 20:28:52 +0100

    Convert security page scripts to guarded ES modules

--- assets/js/overdueVulnerabilitiesForServiceArea.js
+++ assets/js/overdueVulnerabilitiesForServiceArea.js
@@ -1,38 +1,42 @@
-jQuery(async function () {
-  const serviceAreaSlug = document.getElementById('service-area').value
-  $('#updateServiceArea').on('click', async e => {
-    e.preventDefault()
-    const newAreaSlug = document.getElementById('service-area').value
+import { createTable } from './common.js'
 
-    window.location = `/overdue-vulnerabilities/${newAreaSlug}`
-  })
+if (document.querySelector('#products')) {
+  jQuery(async function () {
+    const serviceAreaSlug = document.getElementById('service-area').value
+    $('#updateServiceArea').on('click', async e => {
+      e.preventDefault()
+      const newAreaSlug = document.getElementById('service-area').value
 
-  const columns = [
-    {
-      data: 'name',
-      createdCell: function (td, _cellData, rowData) {
-        $(td).html(
-          `<a href="/overdue-vulnerabilities/${serviceAreaSlug}/product/${rowData.slug}" class="govuk-link--no-visited-state">${rowData.name}</a>`,
-        )
+      window.location = `/overdue-vulnerabilities/${newAreaSlug}`
+    })
+
+    const columns = [
+      {
+        data: 'name',
+        createdCell: function (td, _cellData, rowData) {
+          $(td).html(
+            `<a href="/overdue-vulnerabilities/${serviceAreaSlug}/product/${rowData.slug}" class="govuk-link--no-visited-state">${rowData.name}</a>`,
+          )
+        },
       },
-    },
-    {
-      data: 'numberOfBreachedComponents',
-    },
-    {
-      data: 'totalComponents',
-    },
-    {
-      data: 'numberOfBreachedVulnerabilities',
-    },
-  ]
+      {
+        data: 'numberOfBreachedComponents',
+      },
+      {
+        data: 'totalComponents',
+      },
+      {
+        data: 'numberOfBreachedVulnerabilities',
+      },
+    ]
 
-  createTable({
-    id: 'products',
-    ajaxUrl: `/overdue-vulnerabilities/${serviceAreaSlug}.json`,
-    orderColumn: 0,
-    orderType: 'asc',
-    columns,
-    pageLength: 25,
+    createTable({
+      id: 'products',
+      ajaxUrl: `/overdue-vulnerabilities/${serviceAreaSlug}.json`,
+      orderColumn: 0,
+      orderType: 'asc',
+      columns,
+      pageLength: 25,
+    })
   })
-})
+}
--- assets/js/veracode.js
+++ assets/js/veracode.js
@@ -1,158 +1,5 @@
-const teamFilter = document.getElementById('team')
-const form = document.getElementById('veracodeForm')
-const checkboxes = document.querySelectorAll('input[type="checkbox"]')
+import { createTable, cleanColumnOutput } from './common.js'
 
-let currentFilters = {}
-
-jQuery(function () {
-  currentFilters = getFiltersFromURL()
-
-  // If no filters on load, default to 'failed' results
-  if (areFiltersBlank(currentFilters)) {
-    const failCheckbox = document.querySelector('input[name="results"][value="failed"]')
-    if (failCheckbox) failCheckbox.checked = true
-    currentFilters = getFiltersFromUI()
-    updateURLParams(currentFilters)
-  } else {
-    teamFilter.value = currentFilters.team
-    setCheckboxes(currentFilters)
-  }
-  const rootDataUrl = buildAjaxUrl(currentFilters)
-
-  const columns = [
-    {
-      data: 'name',
-      createdCell: function (td, _cellData, rowData) {
-        $(td).html(`<a href="/components/${cleanColumnOutput(rowData.name)}">${cleanColumnOutput(rowData.name)}</a>`)
-      },
-    },
-    {
-      data: 'result',
-      createdCell: function (td, _cellData, rowData) {
-        let className = 'veracode--missing'
-        let data = 'N/A'
-
-        if (rowData.hasVeracode) {
-          className = rowData.result === 'Passed' ? 'veracode--passed' : 'veracode--failed'
-          data = rowData.result
-        }
-
-        $(td).html(data).addClass(className)
-      },
-    },
-    {
-      data: 'date',
-      title: 'Date',
-      className: 'td-nowrap',
-      render: function (data, type) {
-        if (type === 'display' || type === 'filter') {
-          return data || ''
-        }
-        // sort: use timestamp
-        return Date.parse(data) || 0
-      },
-    },
-    {
-      data: 'severityLevels.VERY_HIGH',
-      title: 'Very High',
-      type: 'num',
-      render: function (data, type, row) {
-        const val = row.hasVeracode ? data : 0
-        if (type === 'display' || type === 'filter') {
-          return row.hasVeracode ? data : 'N/A'
-        }
-        return val
-      },
-    },
-    {
-      data: 'severityLevels.HIGH',
-      title: 'High',
-      type: 'num',
-      render: function (data, type, row) {
-        const val = row.hasVeracode ? data : 0
-        if (type === 'display' || type === 'filter') {
-          return row.hasVeracode ? data : 'N/A'
-        }
-        return val
-      },
-    },
-    {
-      data: 'severityLevels.MEDIUM',
-      title: 'Medium',
-      type: 'num',
-      render: function (data, type, row) {
-        const val = row.hasVeracode ? data : 0
-        if (type === 'display' || type === 'filter') {
-          return row.hasVeracode ? data : 'N/A'
-        }
-        return val
-      },
-    },
-    {
-      data: 'severityLevels.LOW',
-      title: 'Low',
-      type: 'num',
-      render: function (data, type, row) {
-        const val = row.hasVeracode ? data : 0
-        if (type === 'display' || type === 'filter') {
-          return row.hasVeracode ? data : 'N/A'
-        }
-        return val
-      },
-    },
-    {
-      data: 'codeScore',
-      createdCell: function (td, _cellData, rowData) {
-        const data = rowData.hasVeracode ? rowData.codeScore : 0
-        $(td).html(data)
-      },
-      type: 'num',
-    },
-    {
-      data: 'report',
-      createdCell: function (td, _cellData, rowData) {
-        const data = rowData.hasVeracode ? `<a href="${rowData.report}" target="_blank">View</a>` : 'N/A'
-        $(td).html(data)
-      },
-    },
-  ]
-
-  const veracodeTable = createTable({
-    id: 'veracodeTable',
-    ajaxUrl: `${rootDataUrl}`,
-    orderColumn: 6,
-    orderType: 'desc',
-    columns,
-  })
-
-  veracodeTable.on('xhr', function (_e, settings, json) {
-    const rawData = Array.isArray(json) ? json : json.data || []
-    if (!rawData || !rawData.length) return
-
-    populateTeamDropdown(rawData)
-    setCheckboxes(currentFilters)
-
-    $.fn.dataTable.ext.search.push(teamFilterFunction(currentFilters))
-    veracodeTable.draw()
-  })
-
-  // On dropdown update or checkbox change
-  $(
-    '#updateTeam,.environments .govuk-checkboxes__input,.status .govuk-checkboxes__input,.area .govuk-checkboxes__input',
-  ).on('click change', e => {
-    e.preventDefault()
-    //newDataUrl = updateURLParams()
-    currentFilters = getFiltersFromUI()
-    updateURLParams(currentFilters)
-    const newDataUrl = buildAjaxUrl(currentFilters)
-    veracodeTable.ajax.url(newDataUrl).load(() => {
-      $.fn.dataTable.ext.search = [teamFilterFunction(currentFilters)]
-      veracodeTable.draw()
-    })
-  })
-})
-
-// function checks url params for applied filters and builds filter object
 function getFiltersFromURL() {
   const params = new URLSearchParams(location.search)
   return {
@@ -171,7 +18,6 @@ function populateTeamDropdown(data) {
     $teamSelect.append(`<option value="${team}">${team}</option>`)
   })
 
-  // Set value from URL param on load
   if (currentFilters.team) {
     $teamSelect.val(currentFilters.team)
   }
@@ -199,7 +45,6 @@ function getFiltersFromUI() {
   }
 }
 
-// Add current filters to Url params
 function updateURLParams(currentFilters) {
   const newParams = new URLSearchParams()
 
@@ -229,4 +74,157 @@ function setCheckboxes(currentFilters) {
 
 function areFiltersBlank(currentfilters) {
   return currentfilters.team === '' && currentfilters.results.length === 0 && currentfilters.exemption.length === 0
+}
+
+let teamFilter
+let currentFilters = {}
+
+if (document.querySelector('#veracodeTable')) {
+  jQuery(function () {
+    teamFilter = document.getElementById('team')
+    const form = document.getElementById('veracodeForm')
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]')
+
+    currentFilters = getFiltersFromURL()
+
+    if (areFiltersBlank(currentFilters)) {
+      const failCheckbox = document.querySelector('input[name="results"][value="failed"]')
+      if (failCheckbox) failCheckbox.checked = true
+      currentFilters = getFiltersFromUI()
+      updateURLParams(currentFilters)
+    } else {
+      teamFilter.value = currentFilters.team
+      setCheckboxes(currentFilters)
+    }
+    const rootDataUrl = buildAjaxUrl(currentFilters)
+
+    const columns = [
+      {
+        data: 'name',
+        createdCell: function (td, _cellData, rowData) {
+          $(td).html(`<a href="/components/${cleanColumnOutput(rowData.name)}">${cleanColumnOutput(rowData.name)}</a>`)
+        },
+      },
+      {
+        data: 'result',
+        createdCell: function (td, _cellData, rowData) {
+          let className = 'veracode--missing'
+          let data = 'N/A'
+
+          if (rowData.hasVeracode) {
+            className = rowData.result === 'Passed' ? 'veracode--passed' : 'veracode--failed'
+            data = rowData.result
+          }
+
+          $(td).html(data).addClass(className)
+        },
+      },
+      {
+        data: 'date',
+        title: 'Date',
+        className: 'td-nowrap',
+        render: function (data, type) {
+          if (type === 'display' || type === 'filter') {
+            return data || ''
+          }
+          return Date.parse(data) || 0
+        },
+      },
+      {
+        data: 'severityLevels.VERY_HIGH',
+        title: 'Very High',
+        type: 'num',
+        render: function (data, type, row) {
+          const val = row.hasVeracode ? data : 0
+          if (type === 'display' || type === 'filter') {
+            return row.hasVeracode ? data : 'N/A'
+          }
+          return val
+        },
+      },
+      {
+        data: 'severityLevels.HIGH',
+        title: 'High',
+        type: 'num',
+        render: function (data, type, row) {
+          const val = row.hasVeracode ? data : 0
+          if (type === 'display' || type === 'filter') {
+            return row.hasVeracode ? data : 'N/A'
+          }
+          return val
+        },
+      },
+      {
+        data: 'severityLevels.MEDIUM',
+        title: 'Medium',
+        type: 'num',
+        render: function (data, type, row) {
+          const val = row.hasVeracode ? data : 0
+          if (type === 'display' || type === 'filter') {
+            return row.hasVeracode ? data : 'N/A'
+          }
+          return val
+        },
+      },
+      {
+        data: 'severityLevels.LOW',
+        title: 'Low',
+        type: 'num',
+        render: function (data, type, row) {
+          const val = row.hasVeracode ? data : 0
+          if (type === 'display' || type === 'filter') {
+            return row.hasVeracode ? data : 'N/A'
+          }
+          return val
+        },
+      },
+      {
+        data: 'codeScore',
+        createdCell: function (td, _cellData, rowData) {
+          const data = rowData.hasVeracode ? rowData.codeScore : 0
+          $(td).html(data)
+        },
+        type: 'num',
+      },
+      {
+        data: 'report',
+        createdCell: function (td, _cellData, rowData) {
+          const data = rowData.hasVeracode ? `<a href="${rowData.report}" target="_blank">View</a>` : 'N/A'
+          $(td).html(data)
+        },
+      },
+    ]
+
+    const veracodeTable = createTable({
+      id: 'veracodeTable',
+      ajaxUrl: `${rootDataUrl}`,
+      orderColumn: 6,
+      orderType: 'desc',
+      columns,
+    })
+
+    veracodeTable.on('xhr', function (_e, settings, json) {
+      const rawData = Array.isArray(json) ? json : json.data || []
+      if (!rawData || !rawData.length) return
+
+      populateTeamDropdown(rawData)
+      setCheckboxes(currentFilters)
+
+      $.fn.dataTable.ext.search.push(teamFilterFunction(currentFilters))
+      veracodeTable.draw()
+    })
+
+    $(
+      '#updateTeam,.environments .govuk-checkboxes__input,.status .govuk-checkboxes__input,.area .govuk-checkboxes__input',
+    ).on('click change', e => {
+      e.preventDefault()
+      currentFilters = getFiltersFromUI()
+      updateURLParams(currentFilters)
+      const newDataUrl = buildAjaxUrl(currentFilters)
+      veracodeTable.ajax.url(newDataUrl).load(() => {
+        $.fn.dataTable.ext.search = [teamFilterFunction(currentFilters)]
+        veracodeTable.draw()
+      })
+    })
+  })
 }
